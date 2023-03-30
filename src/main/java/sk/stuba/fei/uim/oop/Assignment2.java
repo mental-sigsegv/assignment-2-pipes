527e0661bbfbbbc744663e5a6cceb9bf0f4df00c
Martin Klacik <klacikmartin@protonmail.com>
2023-03-30 11:52:40 +0200

    Water pipes init

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,5 +1,7 @@
 package sk.stuba.fei.uim.oop;
 
+//import sk.stuba.fei.uim.oop.gui.Game;
+
 public class Assignment2 {
     public static void main(String[] args) {
         
---
+++ src/main/java/sk/stuba/fei/uim/oop/gui/Game.java
@@ -0,0 +1,4 @@
+package src.main.java.sk.stuba.fei.uim.oop.gui;
+
+public class Game {
+}
